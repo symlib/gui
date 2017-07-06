1beba3a49272a69e50e97357fabfcb794d6fbf46
symlib <symlib@gmail.com>
2017-07-06 10:51:05 +0800

    Add files via upload
    create snapshot from volume page, from gear button.
    Verify the latest snapshot name from gear button->view detail and volume - collapse pages.

--- create_snapshot.py
+++ create_snapshot.py
@@ -17,8 +17,12 @@ from namegenerator import random_key
 import time
 Pass="'result': 'p'"
 Fail="'result': 'f'"
-
+shortterm=1
+mediumterm=3
+longterm=8
 class CreateSnapshot(unittest.TestCase):
+
+
     def test_create_snapshot(self):
         Failflag = False
         self.driver = loginFirefox()
@@ -26,47 +30,118 @@ class CreateSnapshot(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
         driver = self.driver
-        sleep(1)
-        validatelist = []
+
+
+        sleep(shortterm)
+
+
+        validatelist = list()
         try:
+            tolog("Create snapshot from volume")
             driver.find_element_by_xpath(".//*[@id='sidebar-menu']/ul/li[4]/a").click()
+            sleep(shortterm)
             driver.find_element_by_link_text("Volume").click()
+
             driver.find_element_by_xpath("//div/ul/li[2]/a/span/span").click()  # click v0
-            sleep(1)
+            sleep(shortterm)
             driver.find_element_by_xpath("(//button[@type='button'])[4]").click()  # click "snapshot&clone" button
-            sleep(1)
+            sleep(shortterm)
             driver.find_element_by_xpath(
                 "//pr-button-bar/div/div/div/button[1]").click()  # click 'Create snapshot' button
-            sleep(1)
+            sleep(shortterm)
             driver.find_element_by_name("name").clear()
             snapshot_name = random_key(18)
             driver.find_element_by_name("name").send_keys(snapshot_name)
-            sleep(1)
+            sleep(shortterm)
             driver.find_element_by_xpath("//button[@type='submit']").click()
+
             for i in range(60):
                 try:
                     if re.search(r"^[\s\S]*Snapshot was added successfully.[\s\S]*$",
                                  driver.find_element_by_css_selector("BODY").text):
                         tolog("Snapshot %s was added successfully. " %snapshot_name);
+
                         break
                 except:
                     pass
                 time.sleep(1)
             else:
                 self.fail("time out")
-            a=list()
-            a.append(snapshot_name)
-            validatelist.append(VerifyWords(driver, a))
 
+            sleep(longterm)
+            driver.find_element_by_link_text("Volume").click()
+            sleep(shortterm)
             driver.find_element_by_xpath("//pr-gear-button/div/a").click()
+            sleep(shortterm)
+
             driver.find_element_by_link_text("View Detail").click()
-            sleep(1)
-            validatelist.append(VerifyWords(driver, a))
+            sleep(shortterm)
+            validatelist.append(VerifyWords(driver, ({"Snapshot " + snapshot_name})))
+
+            sleep(shortterm)
+            driver.find_element_by_xpath("//div/ul/li[2]/a/span/span").click()
+            sleep(mediumterm)
+            driver.find_element_by_css_selector("a > small.ng-binding").click()
+            sleep(shortterm)
+            validatelist.append(VerifyWords(driver, ({"Snapshot " + snapshot_name})))
+
+
+            tolog("Create snapshot from gear button")
+            sleep(shortterm)
+            driver.find_element_by_link_text("Volume").click()
+            sleep(shortterm)
+            driver.find_element_by_xpath("//pr-gear-button/div/a").click()
+            sleep(shortterm)
+            driver.find_element_by_link_text("Snapshot & Clone").click()
+            sleep(shortterm)
+
+            driver.find_element_by_xpath("//button[@type='button']").click()
+            sleep(shortterm)
+            driver.find_element_by_name("name").clear()
+            snapshot_name = random_key(18)
+            driver.find_element_by_name("name").send_keys(snapshot_name)
+            sleep(shortterm)
+            driver.find_element_by_xpath("//button[@type='submit']").click()
+            for i in range(60):
+                try:
+                    if re.search(r"^[\s\S]*Snapshot was added successfully.[\s\S]*$",
+                                 driver.find_element_by_css_selector("BODY").text):
+                        tolog("Snapshot %s was added successfully. " %snapshot_name);
+
+                        break
+                except:
+                    pass
+                time.sleep(1)
+            else:
+                self.fail("time out")
+
+            sleep(longterm)
+            driver.find_element_by_link_text("Volume").click()
+            sleep(shortterm)
+            driver.find_element_by_xpath("//pr-gear-button/div/a").click()
+            sleep(shortterm)
+
+            driver.find_element_by_link_text("View Detail").click()
+            sleep(shortterm)
+            validatelist.append(VerifyWords(driver, ({"Snapshot "+snapshot_name})))
+
+            sleep(shortterm)
+            driver.find_element_by_xpath("//div/ul/li[2]/a/span/span").click()
+            sleep(mediumterm)
+            driver.find_element_by_css_selector("a > small.ng-binding").click()
+            sleep(shortterm)
+            validatelist.append(VerifyWords(driver, ({"Snapshot "+snapshot_name})))
+
+
+
+
+
         except:
             driver.get_screenshot_as_file("snapshot at " +
                                           re.sub(':', '.', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(
                                               time.time()))) + "create_delete_multi" + "." + "png")
             tolog("Error: please refer to the screen-shot in the folder")
+
         for val in validatelist:
             if val:
                 Failflag = True
