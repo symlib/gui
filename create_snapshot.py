c81cd721f7af07b39767f4c91c9b7738160839c1
symlib <symlib@gmail.com>
2017-07-06 11:35:11 +0800

    Add files via upload
    added export/unexport snapshot, and modify snapshot

--- create_snapshot.py
+++ create_snapshot.py
@@ -68,6 +68,96 @@ class CreateSnapshot(unittest.TestCase):
             else:
                 self.fail("time out")
 
+
+            sleep(longterm)
+            driver.find_element_by_link_text("Volume").click()
+            sleep(shortterm)
+            driver.find_element_by_xpath("//pr-gear-button/div/a").click()
+            sleep(shortterm)
+
+            driver.find_element_by_link_text("View Detail").click()
+            sleep(shortterm)
+            validatelist.append(VerifyWords(driver, ({"Snapshot " + snapshot_name})))
+
+            sleep(shortterm)
+            driver.find_element_by_xpath("//div/ul/li[2]/a/span/span").click()
+            sleep(mediumterm)
+            driver.find_element_by_css_selector("a > small.ng-binding").click()
+            sleep(shortterm)
+            validatelist.append(VerifyWords(driver, ({"Snapshot " + snapshot_name})))
+
+
+            tolog("Export and Un-export snapshot")
+            sleep(longterm)
+            driver.find_element_by_link_text("Volume").click()
+            sleep(shortterm)
+            driver.find_element_by_xpath("//pr-gear-button/div/a").click()
+            sleep(shortterm)
+            driver.find_element_by_link_text("Snapshot & Clone").click()
+            sleep(2)
+            for i in range(2):
+                status=False
+
+                if re.search("Un-Exported", driver.find_element_by_css_selector("BODY").text):
+                    status=True
+
+                sleep(shortterm)
+                driver.find_element_by_xpath("//pr-gear-button/div/a").click()
+                sleep(shortterm)
+                if status:
+                    driver.find_element_by_link_text("Export Snapshot").click()
+                    for i in range(60):
+                        try:
+                            if re.search(r"^[\s\S]*Snapshot was exported successfully.[\s\S]*$",
+                                         driver.find_element_by_css_selector("BODY").text):
+                                tolog("Snapshot was exported successfully.");
+                                break
+                        except:
+                            pass
+                        time.sleep(1)
+                    else:
+                        self.fail("time out")
+
+                else:
+                    driver.find_element_by_link_text("Un-export Snapshot").click()
+                    for i in range(60):
+                        try:
+                            if re.search(r"^[\s\S]*Snapshot was un-exported successfully.[\s\S]*$",
+                                         driver.find_element_by_css_selector("BODY").text):
+                                tolog("Snapshot was un-exported successfully.");
+                                break
+                        except:
+                            pass
+                        time.sleep(1)
+                    else:
+                        self.fail("time out")
+
+
+            tolog("Modify snapshot")
+            sleep(shortterm)
+            driver.find_element_by_xpath("//pr-gear-button/div/a").click()
+            sleep(shortterm)
+            driver.find_element_by_link_text("Modify Snapshot").click()
+            sleep(shortterm)
+            driver.find_element_by_name("name").clear()
+            snapshot_name = random_key(20)
+            driver.find_element_by_name("name").send_keys(snapshot_name)
+            sleep(shortterm)
+            driver.find_element_by_xpath("//button[@type='submit']").click()
+            sleep(mediumterm)
+            validatelist.append(VerifyWords(driver,({snapshot_name})))
+            for i in range(60):
+                try:
+                    if re.search(r"^[\s\S]*Snapshot was changed successfully.[\s\S]*$",
+                                 driver.find_element_by_css_selector("BODY").text):
+                        tolog("Snapshot was changed successfully..");
+                        break
+                except:
+                    pass
+                time.sleep(1)
+            else:
+                self.fail("time out")
+
             sleep(longterm)
             driver.find_element_by_link_text("Volume").click()
             sleep(shortterm)
@@ -141,6 +231,7 @@ class CreateSnapshot(unittest.TestCase):
                                           re.sub(':', '.', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(
                                               time.time()))) + "create_delete_multi" + "." + "png")
             tolog("Error: please refer to the screen-shot in the folder")
+            Failflag = True
 
         for val in validatelist:
             if val:
